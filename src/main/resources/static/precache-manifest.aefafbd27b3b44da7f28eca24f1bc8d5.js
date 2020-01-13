0b1ca1fe43ad98e1e49ebb4d53cb13cd03783871
desperatecat <376198935@qq.com>
2020-01-12 21:49:19 -0500

    build frontend project to static file and integrate to backend project

---
+++ src/main/resources/static/precache-manifest.aefafbd27b3b44da7f28eca24f1bc8d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c8bb5bfa657b2dc1bce57cee4d4b86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c74c85f549514192f35",
+    "url": "/static/css/2.30a284d3.chunk.css"
+  },
+  {
+    "revision": "1f283b6c1890eea3d1e0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7c74c85f549514192f35",
+    "url": "/static/js/2.6473f291.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.6473f291.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f283b6c1890eea3d1e0",
+    "url": "/static/js/main.94b88924.chunk.js"
+  },
+  {
+    "revision": "73c878cb9d5a7a35e41d",
+    "url": "/static/js/runtime-main.1c3e8da9.js"
+  }
+]);
